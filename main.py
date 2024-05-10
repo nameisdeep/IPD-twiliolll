adfb575c0b6f0ff2b5f161a241734e3b68bb88bc
deepgohil <monisha.g@somaiya.edu>
2024-05-10 19:47:17 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
+from twilio.rest import Client
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+
+
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+# Define a request model
+class SMSRequest(BaseModel):
+    to: str
+    body: str
+
+@app.post("/send-sms")
+async def send_sms(sms_request: SMSRequest):
+    try:
+        message = client.messages.create(
+            body=sms_request.body,
+            to='+918104680835',
+            from_='+18508468650'
+        )
+        return {"success": True, "message_sid": message.sid}
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=str(e))
+
+# Run the server (uncomment the following line if you wish to use uvicorn programmatically)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
